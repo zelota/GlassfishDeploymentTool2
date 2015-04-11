d7cbbd9098c48d64eb8477be715c2f968be3b6f1
zelota <varp@freemail.hu>
2015-04-11 19:30:49 +0200

    Parameter example

--- gdt-maven-plugin/src/main/java/com/seacon/gdt/maven/plugin/GdtMojo.java
+++ gdt-maven-plugin/src/main/java/com/seacon/gdt/maven/plugin/GdtMojo.java
@@ -3,8 +3,8 @@ package com.seacon.gdt.maven.plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 /**
  *
@@ -15,9 +15,12 @@ import org.apache.maven.plugins.annotations.Mojo;
 @Mojo( name = "execgdt")
 public class GdtMojo extends AbstractMojo {
 
+    @Parameter( property = "execgdt.str1", defaultValue = "str1Value" )
+    private String str1;
+    
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        System.out.println("******* GdtMojo is running... **********");
+        System.out.println("******* GdtMojo is running... ********** - str1: " + str1);
     }
     
 }
